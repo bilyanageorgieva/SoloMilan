d5130a7095d65d57559e3d79180a160e5548cc01
Bilyana Georgieva <bibby.georgieva@gmail.com>
2019-09-14 13:33:29 +0200

    feat: make big featured article get data dynamically

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Category names in Home Page
 {
   // Retrieve the template data from the HTML
-  let template = $("#categories-list").html();
+  let template = $("#tmpl-categories-list").html();
 
   // Create the context
   let context = {
-    categories: Object.values(categories).map(function (x) {
+    categories: Object.values(categories).map(function(x) {
       return { name: x };
     })
   };
@@ -15,4 +15,29 @@
   let html = templateScript(context);
 
   $("nav").append(html);
+}
+
+// Big featured article
+{
+  // Retrieve the template data from the HTML
+  let template = $("#tmpl-big-featured-post").html();
+
+  // Get the first featured article
+  let featured = articles.find(function(x) {
+    return x.featured;
+  });
+
+  // Create the context
+  let context = { title: featured.title, subtitle: featured.subtitle };
+
+  // Compile the template data into a function
+  let templateScript = Handlebars.compile(template);
+  let html = templateScript(context);
+
+  $("#big-featured-post").prepend(html);
+}
+
+// Two small featured articles
+{
+  
 }
